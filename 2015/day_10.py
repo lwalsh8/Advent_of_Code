ad9ab5d68c42e91afbb61525b89b2ad9aae90247
Walsh <lwalsh6@LAMU02W63W3HTD6.uhc.com>
2021-04-14 11:50:34 +0100

    Adding solution for Day 10

---
+++ 2015/day_10.py
@@ -0,0 +1,42 @@
+import sys
+from itertools import groupby
+from termcolor import colored
+import datetime
+start_time = datetime.datetime.now()
+
+
+def update_text(first_str, total_iter):
+    def recursive_bit(iter_num, updated_str):
+        if iter_num == total_iter:
+            return updated_str
+        
+        iter_num +=1
+        updated_str = "".join("{}{}".format(len(list(count)), int(label)) for label, count in groupby(updated_str))
+        return recursive_bit(iter_num, updated_str)
+    
+    return recursive_bit(0, first_str)
+        
+
+def main(puzzle_input, part_num):
+    first_str = puzzle_input
+    if part_num == '1':
+        total_iter = 40
+    elif part_num == '2':
+        total_iter = 50
+    
+    print(colored('Length of final string: {}'.format(len(update_text(first_str, total_iter))), "green"))
+    processing_time = (datetime.datetime.now() - start_time).total_seconds()
+    print(colored("Time taken to get answer: {:.3f} seconds".format(processing_time), "green"))
+    
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        puzzle_input = sys.argv[1]
+    else:
+        puzzle_input = '1113222113'
+        
+    if len(sys.argv) > 2:
+        part_num = sys.argv[2]
+    else:
+        part_num = '1'
+    main(puzzle_input, part_num)
